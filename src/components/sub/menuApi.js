3e38687a793e2877ad5ba968b4f5ac7c27143818
Aryann15 <rajlysm15@gmail.com>
2023-04-07 19:16:13 +0530

    adding the half of the API for the react Application

---
+++ src/components/sub/menuApi.js
@@ -0,0 +1,83 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/beet_pancakes.jpg",
+    name: "Beet Pancakes",
+    category: "Breakfast",
+    price: 5,
+    description:
+      "Beets are a superfood! My beet pancakes are packed with vitamins and minerals, making them a healthy and nutritious choice for breakfast.",
+  },
+  {
+    id: 2,
+    image: "images/hard-boiled-egg-sardines.jpg",
+    name: "Hard Boiled Eggs and Sardines",
+    category: "Breakfast",
+    price: 1,
+    description:
+      "Eggs and sardines, the breakfast of champions! Channel your inner Schrute strength with this protein-packed duo that will keep you energized and ready to tackle the day ahead. Join me in embracing the power of sardines!",
+  },
+  {
+    id: 3,
+    image: "images/black coffee.jpg",
+    name: "Black Coffee with a side of Schrute Farms' Maple Syrup",
+    category: "Breakfast",
+    price: 1,
+    description:
+      "As a true coffee connoisseur, I highly recommend trying black coffee with Schrute Farms' maple syrup! It's a sophisticated and refined choice that will make your taste buds sing and impress anyone with your discerning palate",
+  },
+  {
+    id: 4,
+    image: "images/battlestar-galactica-themed-breakfast.jpg",
+    name: "Battlestar Galactica-themed Breakfast",
+    category: "Breakfast",
+    price: 1,
+    description:
+      "As a true coffee connoisseur, I highly recommend trying black coffee with Schrute Farms' maple syrup! It's a sophisticated and refined choice that will make your taste buds sing and impress anyone with your discerning palate",
+  },
+  {
+    id: 5,
+    image: "images/beet salad.jpg",
+    name: "Schrute Farms' Beet Salad",
+    category: "Breakfast",
+    price: 1,
+    description:
+      "Experience the taste sensation of a beet salad for lunch! The combination of tender beet slices, crisp veggies, and a zesty dressing is a symphony of flavors that will awaken your taste buds. It's a salad that's as satisfying as it is delicious, and a perfect addition to any lunchtime meal.",
+  },
+  {
+    id: 6,
+    image: "images/Grilled Meat Sandwich.jpg",
+    name: "Grilled Meat Sandwich",
+    category: "Lunch",
+    price: 1,
+    description:
+      "Join me in a lunchtime feast with a scrumptious grilled meat sandwich! From a classic cheeseburger to a gourmet club sandwich, the grilled meat brings out the best flavors and textures in this lunchtime favorite. Bite into a sandwich that's grilled to perfection and discover the joy of a truly satisfying meal!",
+  },
+  {
+    id: 7,
+    image: "images/Rainn Wrap.jpg",
+    name: "Rainn Wrap",
+    category: "Lunch",
+    price: 1,
+    description:
+      "Craving a lunch that's as unique as you are? Try a Rainnwrap for a taste sensation like no other! With its unconventional combination of ingredients and bold flavors, it's a wrap that's sure to impress and leave you wanting another bite. Don't miss out on this gastronomic adventure!",
+  },
+  {
+    id: 8,
+    image: "images/Beet Burger.jpg",
+    name: "Beet Burger",
+    category: "Lunch",
+    price: 1,
+    description:
+      "Looking for a healthier lunch option? Try a beet burger for a guilt-free meal that doesn't skimp on flavor! The natural sweetness and earthy flavors of beets, combined with the satisfying textures of the burger patty, make for a unique and enjoyable eating experience.",
+  },
+  {
+    id: 9,
+    image: "images/Schrute's Special Stew.jpg",
+    name: "Schrute's Special Stew",
+    category: "Lunch",
+    price: 1,
+    description:
+      "Craving a stew that's made with love and attention to detail? Look no further than Schrute's Special Stew! It's a labor of love from Schrute Farms, made with the freshest ingredients and cooked to perfection. Treat yourself to a bowl of this special stew and discover the true essence of comfort food.",
+  },
+  
